aea53137fd8cfa42b4f629919b880f0f2c921521
alendez5 <alejandro.menendezcanache@tiernoparla.es>
2022-11-26 22:01:48 +0100

    añado a Resta

---
+++ src/Resta.java
@@ -0,0 +1,21 @@
+
+public class Resta {
+	
+	private int x;
+	private int y;
+	
+	public Resta(int x, int y) {
+		
+		this.x = x;
+		this.y = y;
+		
+	}
+	
+	
+	public int resultado() {
+		return x - y;
+	}
+	
+
+
+}
